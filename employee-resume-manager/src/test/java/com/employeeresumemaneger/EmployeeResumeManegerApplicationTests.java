25dc835fdfd185b54be728005f6b5d2462477669
dogankaya <dogan.kaya@ogr.sakarya.edu.tr>
2026-07-14 11:15:50 +0300

    directory name changed

---
+++ employee-resume-manager/src/test/java/com/employeeresumemaneger/EmployeeResumeManegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeresumemaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeResumeManegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
